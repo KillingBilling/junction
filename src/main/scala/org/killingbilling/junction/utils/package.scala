64a7aa919c455eb4753b38a5633ff583517ff48c
Ivan Mikushin <i.mikushin@gmail.com>
2014-01-11 16:28:39 +0600

    minor formatting

--- src/main/scala/org/killingbilling/junction/utils/package.scala
+++ src/main/scala/org/killingbilling/junction/utils/package.scala
@@ -5,9 +5,7 @@ import javax.script.{ScriptEngine, ScriptEngineManager}
 
 package object utils {
 
-  def jf[A, B](f: Function[A, B]) = new function.Function[A, B] {
-    def apply(t: A) = f(t)
-  }
+  def jf[A, B](f: Function[A, B]) = new function.Function[A, B] {def apply(t: A) = f(t)}
 
   implicit def sf[A, B](f: function.Function[A, B]): A => B = f.apply
 
