434dcff818010828ba8216c1f71d1574b25bd97d
Ivan Mikushin <i.mikushin@gmail.com>
2014-01-12 20:28:09 +0600

    refactor Require

--- src/main/scala/org/killingbilling/junction/Require.scala
+++ src/main/scala/org/killingbilling/junction/Require.scala
@@ -6,7 +6,7 @@ import org.killingbilling.junction.utils._
 trait Require {
   def resolve(path: String): String
   def getCache: JMap[String, Module]
-  protected def module(path: String): Module
+  def module(path: String): Module
 
   def impl[T](path: String, c: Class[T]): T
 
@@ -17,11 +17,11 @@ object Require extends Require {
 
   private lazy val default: Require = forModule(new Module())
 
+  def forModule(module: Module): Require = module.getRequire
+
   def getCache = default.getCache
   def resolve(path: String) = default.resolve(path)
-  protected def module(path: String) = default.module(path)
+  def module(path: String) = default.module(path)
   def impl[T](path: String, t: Class[T]) = default.impl(path, t)
-
-  def forModule(module: Module): Require = module.getRequire
 
 }
